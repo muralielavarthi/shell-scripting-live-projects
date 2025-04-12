56cd7a4d49941d138a3f46d45821c0154a3b7c28
Murali Elavarthi <muralis2p@gmail.com>
2025-04-12 17:00:03 +0530

    c2

--- one.sh
+++ one.sh
@@ -2,9 +2,20 @@
 
 TIMESTAMP=$(date +"%m-%d-%y-%H-%M")
 FILE_NAME="Users+$TIMESTAMP"
+USER_ID=$UID
+
+function rootValidate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Root access is required"
+        exit 1
+    fi
+}
+
+rootValidate $USER_ID
+
 
 mkdir -p /root/Users_Login_Data/
-
 who | awk '{print $1,$3,$4,$5}' >>/root/Users_Login_Data/$FILE_NAME
 
 
