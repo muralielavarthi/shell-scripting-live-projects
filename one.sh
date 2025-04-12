65ce3654f1dc3868835866299e5e48c22dd2b25e
Murali Elavarthi <muralis2p@gmail.com>
2025-04-12 16:36:51 +0530

    c2

--- one.sh
+++ one.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+TIMESTAMP=$(date +"%m-%d-%y-%H-%M")
+FILE_NAME="Users+$TIMESTAMP"
+
+mkdir -p /root/Users_Login_Data/
+
+who | awk '{print $1,$3,$4,$5}' >>/root/Users_Login_Data/$FILE_NAME
+
+
+
