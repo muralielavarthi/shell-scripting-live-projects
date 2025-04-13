7bf22ca2fbe2fa2192e09c6f3209b424338118fe
Murali Elavarthi <muralis2p@gmail.com>
2025-04-13 05:44:44 +0530

    c3

--- two.sh
+++ two.sh
@@ -19,7 +19,7 @@ while read line
 do
     echo $line >> /root/Users_Login_Data/delete.log
     rm -rf $line
-done <<< old_files.txt
+done < old_files.txt
 
 # -mmin option checks the file's modification time, which refers to the last time the contents of the file were changed
 # -cmin stands for "change", specifically change of file status (not content).
