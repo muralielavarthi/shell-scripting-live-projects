6973fa98a1bb408a8395d523636160d95f092361
Murali Elavarthi <muralis2p@gmail.com>
2025-04-14 05:32:46 +0530

    c4

---
+++ three.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 #real projects will monitor for 70
+MSG=""
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1) 
+    # from 50% value we are cutting % since it can't be compared with number
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    #echo "Partition: $PARTITION , Usage: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n " 
+        # \n for output to come in new line
+        # + to append value variable instead of replacing the content
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $MSG"
+
+echo "$MSG" | mutt -s "High Disk Usage" info@joindevops.com # sending email(-s:subject line)
